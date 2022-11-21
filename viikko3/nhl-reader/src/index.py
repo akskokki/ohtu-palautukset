cf9946f6d9d4253611ccdc00be60e5a8d586dc39
akskokki <akseli.kokki@gmail.com>
2022-11-21 13:35:41 +0200

    v3t1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,24 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        if player_dict['nationality'] == 'FIN':
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+            players.append(player)
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
