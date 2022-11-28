48ccdbaee3f4625259a0c4a0f48695de54547452
akskokki <akseli.kokki@gmail.com>
2022-11-28 10:48:42 +0200

    v4t2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_voidaan_ladata_positiivinen_summa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+    
+    def test_kortille_ei_voida_ladata_negatiivinen_summa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
