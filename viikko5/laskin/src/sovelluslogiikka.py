4e188b560cfbeb8d2b58eb77693ef0949d261452
akskokki <akseli.kokki@gmail.com>
2022-12-06 21:20:14 +0200

    v5t5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,22 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.prev = 0
 
     def miinus(self, arvo):
+        self.prev = self.tulos
         self.tulos = self.tulos - arvo
 
     def plus(self, arvo):
+        self.prev = self.tulos
         self.tulos = self.tulos + arvo
 
     def nollaa(self):
+        self.prev = self.tulos
         self.tulos = 0
 
     def aseta_arvo(self, arvo):
         self.tulos = arvo
+
+    def kumoa(self):
+        self.tulos = self.prev
